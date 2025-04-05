9ac764f9ae2f6bc8e743e6ff10c30eb07389d42b
Sandeep S <ssandeep.5391@gmail.com>
2025-04-05 21:19:04 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -9,30 +9,68 @@ import org.openqa.selenium.logging.LoggingPreferences;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    Wrappers wrapper ;
+    LocalDate today = LocalDate.now();
+    LocalDate sevenDaysAgo = today.minusDays(7);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    long epoch = System.currentTimeMillis() / 1000;
+    private String url ="https://forms.gle/wjPkzeSEk1CM7KgGA";
+
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
+    @Test
+    public void testCase01() throws InterruptedException {
+        //launching form
+        wrapper.openURL(url);
+        Thread.sleep(2000);
+        //filling form
+        List<WebElement> inputbox = driver.findElements(By.xpath("//input[@type='text']"));
+        wrapper.typeText(inputbox.get(0), "Crio Learner");
+        WebElement autobox = driver.findElement(By.xpath("//textarea[@jsname='YPqjbf']"));;
+        wrapper.typeText(autobox, "I want to be the best QA Engineer! " + epoch);
+        WebElement radio = driver.findElement(By.xpath("(//div[@class='SG0AAe']/div[3]//div/div)[5]"));
+        wrapper.clickElement(radio);
+        List<WebElement> checkboxes = driver.findElements(By.xpath("//div[@class='eBFwI']"));
+        wrapper.clickElement(checkboxes.get(0));
+        wrapper.clickElement(checkboxes.get(1));
+        wrapper.clickElement(checkboxes.get(3));
+        WebElement dropElement = driver.findElement(By.xpath("//span[text()='Choose']"));
+        wrapper.clickElement(dropElement);
+        Thread.sleep(1000);
+        wrapper.selectPronoun(By.xpath("//div[@jsname='V68bde']/div[@jsname='wQNmvb'][2]/span"));
+        Thread.sleep(1000);
+        WebElement date = driver.findElement(By.xpath("//input[@type='date']"));
+        wrapper.setDate(date, sevenDaysAgo.format(formatter));
+        wrapper.typeText(inputbox.get(1), "07");
+        wrapper.typeText(inputbox.get(2), "30");
+        // submitting form
+        wrapper.clickSubmit(By.xpath("(//div[@role='button'])[1]"));
+        //checking succes message
+        wrapper.displayText(By.xpath("//div[@class='vHW8K']"));
+    }
 
-     
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
-
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
@@ -42,16 +80,17 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        wrapper = new Wrappers(driver);
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,9 +3,11 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.logging.Logger;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,4 +18,49 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    ChromeDriver driver;
+    WebDriverWait wait ;
+    Logger logger = Logger.getLogger(Wrappers.class.getName());
+
+    public Wrappers(ChromeDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void openURL(String url) {
+        driver.get(url);
+    }
+
+    public void clickElement(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    public void typeText(WebElement element, String s) {
+        element.click();
+        element.clear();
+        element.sendKeys(s);
+    }
+
+    public void displayText(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        WebElement string = driver.findElement(locator);
+        System.out.println(string.getText());
+    }
+
+    public void selectPronoun(By pro) {
+        WebElement pr = driver.findElement(pro);
+        pr.click();
+        System.out.println("Pronoun Selected");   
+    }
+
+    public void setDate(WebElement date, String format) {
+        date.sendKeys(format);
+        System.out.println("Date Selected");  
+    }
+
+    public void clickSubmit(By xpath) {
+        WebElement element = driver.findElement(xpath);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element);
+        element.click();
+    }
 }
